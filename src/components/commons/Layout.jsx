e7f845562fe12a04d518d2fbfb4d8dc7a254ffbd
Angelo Leva <angelokta7@gmail.com>
2020-10-12 12:22:28 -0500

    added wsp button

--- src/components/commons/Layout.jsx
+++ src/components/commons/Layout.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Nav from "./Nav/Nav";
 import Footer from "./Footer/Footer";
+import Whatsapp from "./Wsp/Whatsapp";
 
 const Layout = ({ children }) => (
   <div className="app">
     <Nav />
     {children}
+    <Whatsapp />
     <Footer />
   </div>
 );
---
+++ src/components/commons/Wsp/Whatsapp.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Whatsapp = () => {
+  return (
+    <div className="whatsapp-cta">
+      <a target="_blank" href="https://wa.link/fkwizg">
+        <span className="icon icon-whatsapp"></span>
+      </a>
+    </div>
+  );
+};
+
+export default Whatsapp;
