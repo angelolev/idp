e1fc6b96be8bafcec402030794f6ca20fb459c69
Angelo Leva <angelokta7@gmail.com>
2020-10-14 20:09:02 -0500

    added whatsapp link for desktop

--- src/components/commons/Wsp/Whatsapp.jsx
+++ src/components/commons/Wsp/Whatsapp.jsx
@@ -3,7 +3,18 @@ import React from "react";
 const Whatsapp = () => {
   return (
     <div className="whatsapp-cta">
-      <a target="_blank" href="https://wa.link/fkwizg">
+      <a
+        class="whatsapp-cta__mobile"
+        target="_blank"
+        href="https://wa.link/fkwizg"
+      >
+        <span className="icon icon-whatsapp"></span>
+      </a>
+      <a
+        class="whatsapp-cta__desktop"
+        target="_blank"
+        href="https://web.whatsapp.com/send?phone=51975758095&text="
+      >
         <span className="icon icon-whatsapp"></span>
       </a>
     </div>
